2d2a11b4dbf6ab4ed4ac6ee4096b6f83dbd50e05
Immortal9100 <himanshulahare80@gmail.com>
2020-07-26 21:56:00 +0530

    BookAppointment redisgn

--- src/app/bookappointment/bookappointment.component.ts
+++ src/app/bookappointment/bookappointment.component.ts
@@ -1,9 +1,9 @@
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { NgbCalendar, NgbDate,} from '@ng-bootstrap/ng-bootstrap';
+import { NgbCalendar, NgbDate, NgbDateStruct,} from '@ng-bootstrap/ng-bootstrap';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
-import { equal } from 'assert';
+
 declare var $: any;
 
 
@@ -17,13 +17,13 @@ export class BookappointmentComponent implements OnInit {
   emailPattern="(^$|^.*@.*\..*$)";
   phnopattern="[0-9]{10}";
   emailempty:boolean=true;
-  _model: NgbDate;
+  model: NgbDateStruct;
   date: {year: number, month: number};
   dateselected:boolean=false;
   timeselected:boolean=false;
   checkavailabilty:boolean=true;
   selecteddatestring;
-  selecteddatestringwithoutslash;
+  
   
   weekDays = {
     1: 'Monday',
@@ -34,40 +34,17 @@ export class BookappointmentComponent implements OnInit {
     6: 'Satarday',
     7: 'Sunday'
   }
-  selectedDay: string = '';
-  set model(val) {
-    this._model = val;
-    this.selectedDay = this.weekDays[this.calendar.getWeekday(this.model)]
-  }
-
-  get model() {
-    return this._model;
-  }
- 
-
  
   selectedate;
 
-  time;
 
-  settime(t:String){
-   
-    this.time=t;
-    this.timeselected=true;
-    console.log(this.time);
-   
-  }
-  onDateSelect(asd){
-    this.selectedate=this.model.day;
-    this.dateselected=true;
-    console.log(this.selectedate);
-    console.log("Date seleted");
-  }
+
   constructor(private calendar: NgbCalendar,public fb: FormBuilder) {
     this.appointmentform = this.fb.group({
       name: ['', [Validators.required]], 
       email: ['',[Validators.pattern(this.emailPattern)]],
       phno:['', [Validators.required, Validators.pattern(this.phnopattern)]],
+      dates:[''],
       msg: [''],
       subject:[''], 
       confirmphno: ['', [Validators.required]]
@@ -119,67 +96,24 @@ export class BookappointmentComponent implements OnInit {
   closed:boolean=false;
   closedmsg:String="";
   checkavailablilticlicked:boolean=false;
-  async checkclosed(){
-    this.closed=false;
-    this.checkavailablilticlicked=true;
-    //check the date if it is closed or not
-    //create a date string with selectdate
-    this.selecteddatestring=this.selectedate.toString() +"/" +this.date.month.toString()+"/"+this.date.year.toString();
-    this.selecteddatestringwithoutslash=this.selectedate.toString()  +this.date.month.toString()+this.date.year.toString();
-    console.log(this.selecteddatestring);
-    console.log(this.selecteddatestringwithoutslash);
-    console.log("started");
-    await firebase.firestore().collection('closeddate').doc(this.selecteddatestringwithoutslash).get().then((ds)=>{
-      if (ds.exists) {
-        console.log(ds.data.length);
-      this.closedday=ds.data();
-      this.closed=true;
-      this.closedmsg=this.closedday.closedmsg;
-      console.log(this.closedday.closedmsg);
-      console.log("Document data:", this.closedday.closedmsg); 
-            } else {
-                      console.log("No such document!");
-    }
-      
-    }).catch((ee)=>{"ERROR"+ee})
 
-       //check the selectedday if it is closed or not
-   await firebase.firestore().collection("closedday").doc("closedday").get().then((document)=>{
-    this.closedday=document.data();
-    console.log(this.closedday.day);
-  }).catch((err)=>{
-    console.log("Error occured"+err);
-  });
-  if(this.selectedDay==this.closedday.day){
-    this.closed=true;
-    this.closedmsg="We are closed on Sunday"
-    console.log("We are CLosed on Sunday ");
-  }
-  if(this.closed==false){
-    this.closedmsg="Available";
-  }
-  
- 
-  }
+
   onSubmit(appointmentform){
     console.log('Form Submitted');
+    this.selecteddatestring=this.model.day+"/"+this.model.month+"/"+this.model.year;
     let email: string = appointmentform.value.email;
     let name: string = appointmentform.value.name;
     let phno: string = appointmentform.value.phno;
-    let msg: string = appointmentform.value.msg;
-    let subject: string=appointmentform.value.subject;
 
-    console.log(email,name,phno,msg);
+    console.log(email,name,phno,this.selecteddatestring);
     
   
     firebase.firestore().collection("bookappointment").doc().set({
       name:appointmentform.value.name,
       phno:appointmentform.value.phno,
       email:appointmentform.value.email,
-      msg:appointmentform.value.msg,
-      subject:appointmentform.value.subject,
       appointmentdate:this.selecteddatestring,
-      appointmenttime:this.time
+    
     }).then(()=>{
       
       
@@ -190,6 +124,7 @@ export class BookappointmentComponent implements OnInit {
 
     }).catch((err)=>{console.log("err")});
      appointmentform.reset();
+   
     
   }
  
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,52 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
+
 export class GalleryComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Fetching links");
+    
+    this.getgallarylinks();
+
+    console.log("Done");
   }
+
+
+  linkobject:any;
+  links:  Array<any> = [];
+  linkobjectsize;
+  async getgallarylinks(){
+  await firebase.firestore().collection("gallary").doc("thumbnail").get().then((document)=>{
+    this.linkobject=document.data();
+    this.linkobjectsize=document.data().gsize;
+    console.log(this.linkobject);
+    console.log(this.linkobjectsize);
+  
+  }).catch((err)=>{
+    console.log("Error occured"+err);
+  });
+ 
+
+  for(let i=1;i<=this.linkobjectsize;i++){
+   let v=this.linkobject["g"+i];
+   if(v!=null){
+    this.links.push(v);
+   }
+    console.log(v);
+    
+    console.log(this.links);
+  }
+}
+
 
 }
